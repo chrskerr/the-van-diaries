5f33e44ec4f9026123ecea9d40e88bff8ba11dd0
Chris Kerr <chriskerr@me.com>
2021-03-16 17:01:52 +1100

    updated to allow build in cloudflare

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -3,6 +3,7 @@
     export let zoom;
     export let centre;
 
+    import { browser } from "$app/env";
     import { onMount } from "svelte";
     import _ from "lodash";
     import "../../node_modules/leaflet/dist/leaflet.css";
@@ -11,31 +12,33 @@
     const markers = [{ name: "Derby", latLng: [-41.14213260110557, 147.79722207630516] }];
 
     onMount(async () => {
-        const L = await import("leaflet");
-        const GeoSearch = await import("leaflet-geosearch");
-        await import("leaflet-providers");
+        if (browser) {
+            const L = await import("leaflet");
+            const GeoSearch = await import("leaflet-geosearch");
+            await import("leaflet-providers");
 
-        const map = L.map("map").setView(centre, zoom);
-        L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        }).addTo(map);
-        setTimeout(() => map.invalidateSize(), 50);
+            const map = L.map("map").setView(centre, zoom);
+            L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+            }).addTo(map);
+            setTimeout(() => map.invalidateSize(), 50);
 
-        map.addControl(
-            new GeoSearch.GeoSearchControl({
-                provider: new GeoSearch.OpenStreetMapProvider(),
-                style: "bar",
-            }),
-        );
+            map.addControl(
+                new GeoSearch.GeoSearchControl({
+                    provider: new GeoSearch.OpenStreetMapProvider(),
+                    style: "bar",
+                }),
+            );
 
-        const icon = L.divIcon({ html: "<span class='feather-location' />" });
-        _.forEach(markers, ({ name, latLng }) => {
-            const marker = L.marker(latLng, { icon }).addTo(map).bindTooltip(name, { direction: "top" });
+            const icon = L.divIcon({ html: "<span class='feather-location' />" });
+            _.forEach(markers, ({ name, latLng }) => {
+                const marker = L.marker(latLng, { icon }).addTo(map).bindTooltip(name, { direction: "top" });
 
-            marker.on("click", e => {
-                console.log(e);
+                marker.on("click", e => {
+                    console.log(e);
+                });
             });
-        });
+        }
     });
 </script>
 
