c44b89b7b714ff965f66f5be282a4a4daa0c3cdb
Chris Kerr <chriskerr@me.com>
2021-03-18 08:54:48 +1100

    Updated mobile view

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { afterUpdate, onMount } from "svelte";
+    import { afterUpdate } from "svelte";
     import _ from "lodash";
 
     let pathname;
@@ -7,67 +7,40 @@
         pathname = window.location.pathname;
     });
 
-    let width;
-    onMount(() => {
-        width = window.innerWidth;
-        window.addEventListener("resize", () => (width = window.innerWidth));
-    });
+    const _toggleNav = () => {
+        document.body.classList.toggle("is-navPanel-visible");
+        document.getElementById("navPanel").classList.add("toggleable");
+    };
 
     let navOpen = false;
 </script>
 
 <header id="header" class="alt">
     <a class="logo" href="/">The Van Diaries <span>by Kate & Chris</span></a>
-    {#if !width || width > 980}
-        <nav id="nav">
-            <ul>
-                <li class={pathname === "/" ? "current" : ""}>
-                    <a href="/">Map</a>
-                </li>
-                <li class={_.startsWith(pathname, "places") ? "current" : ""}>
-                    <a href="/places">Places</a>
-                </li>
-            </ul>
-        </nav>
-    {:else}
-        <span class="nav-button feather-menu" on:click={() => (navOpen = !navOpen)}>
-            <div id="navPanel" class={navOpen ? "open" : ""}>
-                <span class="nav-close feather-x" />
-                <nav>
-                    <a class="link depth-0" href="/">Map</a>
-                    <a class="link depth-0" href="/places">Places</a>
-                </nav>
-            </div>
-        </span>
-        {#if navOpen}
-            <div class="nav-overlay" on:click={() => (navOpen = !navOpen)} />
-        {/if}
-    {/if}
+    <nav id="nav">
+        <ul>
+            <li class={pathname === "/" ? "current" : ""}>
+                <a href="/">Map</a>
+            </li>
+            <li class={_.startsWith(pathname, "places") ? "current" : ""}>
+                <a href="/places">Places</a>
+            </li>
+        </ul>
+    </nav>
+    <span class="nav-button feather-menu" on:click={_toggleNav} />
 </header>
 
 <style>
-    #navPanel {
-        text-align: left;
+    .nav-button {
+        display: none;
     }
 
-    .nav-close {
-        position: absolute;
-        top: 1rem;
-        right: 1rem;
-        color: white;
-    }
-
-    #navPanel.open {
-        transform: translateX(0%);
-        visibility: initial;
-    }
-
-    .nav-overlay {
-        position: absolute;
-        top: 0;
-        bottom: -100vh;
-        left: 0;
-        right: 0;
-        z-index: 100;
+    @media screen and (max-width: 480px) {
+        #nav {
+            display: none;
+        }
+        .nav-button {
+            display: initial;
+        }
     }
 </style>
